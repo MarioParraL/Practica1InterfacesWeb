43de9f7acf817dee0f8e7a7806bf1abc975456e8
MarioParraL <mariovidovick@gmail.com>
2024-03-03 19:28:08 +0100

    Practica-1

--- Practica-1/routes/index.tsx
+++ Practica-1/routes/index.tsx
@@ -1,28 +1,45 @@
 import Axios from "npm:axios";
+import Button from "../components/Button.tsx";
 type Data = {
   phrase: string;
 };
 
+type Episode = {
+  id: string;
+  thumbnailUrl: string;
+};
+
 export default async function Home() {
-  const nRandom: number = Math.floor(Math.random() * 40) + 1;
   const films = await Axios.get<Data>(
     "https://filmquotes.deno.dev/",
   );
 
+  const randomNumber2 = Math.floor(Math.random() * 100) + 1;
+  const randomResponse2 = await Axios.get<Episode>(
+    `https://api.sampleapis.com/simpsons/episodes/${randomNumber2}`,
+  );
+
+  const episode = randomResponse2.data;
   return (
-    <body>
-      <audio controls loop>
+    <div class="principal">
+      <div class="tittle">HEY! THE BEST FILM CUOTES IN YOUR SCREEN!</div>
+      <audio class="miaudio" controls loop>
         <source src="TRS.mp3" />
       </audio>
 
-      <h1 class="tittle">Welcome, play the music & enjoy</h1>
-      <h2 class="subtittle">A wise man / woman once said:</h2>
-      <h3 class="phrase">{films.data}</h3>
-
+      <h2 class="phrase">{films.data}</h2>
+      <div class="info">Click play to listen</div>
+      <img class="imagenApi" src={episode.thumbnailUrl} />
       <img class="gif" src="gifTitanic.gif" />
-      <button class="button">Touch here</button>
-      <div>
+
+      <div class="buttons">
+        <a href="https://github.com/MarioParraL">
+          <Button variant="primary">MY GitHub</Button>
+        </a>
+        <a href="https://www.filmaffinity.com/es/ranking.php?rn=ranking_fa_movies">
+          <Button variant="secondary">TOP HISTORY MOOVIES</Button>
+        </a>
       </div>
-    </body>
+    </div>
   );
 }
